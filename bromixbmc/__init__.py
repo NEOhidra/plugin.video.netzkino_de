d5f8a3594239a0082c1872a43bc09884a74f887d
bromix <bromix@gmx.net>
2014-07-09 00:44:30 +0200

    moved to a renewed version of bromixbmc

--- bromixbmc/__init__.py
+++ bromixbmc/__init__.py
@@ -1,2 +1,31 @@
-from bromixbmc import Bromixbmc
-from bromixbmc import Addon
+"""
+Version 2.0.1 (2014.07.08)
+- complete restructuring
+
+Version 1.0.2 (2014.06.25)
+- added 'getFavorites', 'addFavorite' and 'removeFavorite'
+- set the encoding for the favorite routines to utf-8
+- removed 'duration' and 'plot' from addVideoLink -> therefore use 'additionalInfoLabels'
+
+Version 1.0.1 (2014.06.24)
+- added support for helping with favorites
+- initial release
+"""
+
+import re
+import sys
+import urlparse
+
+from plugin import Plugin
+from keyboard import Keyboard
+
+def stripHtmlFromText(text):
+    return re.sub('<[^<]+?>', '', text)
+
+def getParam(name, default=None):
+    args = urlparse.parse_qs(sys.argv[2][1:])
+    value = args.get(name, None)
+    if value and len(value)>=1:
+        return value[0]
+    
+    return default
--- default.py
+++ default.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import xbmcplugin
-import xbmcgui
-import xbmcaddon
-
 import os
 import re
 import json
@@ -13,18 +9,19 @@ import urllib
 #import pydevd
 #pydevd.settrace('localhost', stdoutToServer=True, stderrToServer=True)
 
-from bromixbmc import Bromixbmc
-bromixbmc = Bromixbmc("plugin.video.netzkino_de", sys.argv)
+import bromixbmc
 
-__FANART__ = os.path.join(bromixbmc.Addon.Path, "fanart.jpg")
-__ICON_HIGHLIGHTS__ = os.path.join(bromixbmc.Addon.Path, "resources/media/highlight.png")
-__ICON_SEARCH__ = os.path.join(bromixbmc.Addon.Path, "resources/media/search.png")
+__plugin__ = bromixbmc.Plugin()
+
+__FANART__ = os.path.join(__plugin__.getPath(), "fanart.jpg")
+__ICON_HIGHLIGHTS__ = os.path.join(__plugin__.getPath(), "resources/media/highlight.png")
+__ICON_SEARCH__ = os.path.join(__plugin__.getPath(), "resources/media/search.png")
 
 __ACTION_SHOW_CATEGORY__ = 'showCategory'
 __ACTION_SEARCH__ = 'search'
 __ACTION_PLAY__ = 'play'
 
-__SETTING_SHOW_FANART__ = bromixbmc.Addon.getSetting('showFanart')=="true"
+__SETTING_SHOW_FANART__ = __plugin__.getSettingAsBool('showFanart')
 if not __SETTING_SHOW_FANART__:
     __FANART__ = ''
 
@@ -63,17 +60,17 @@ def _listCategories(json_categories):
     for category in json_categories:
         id = category.get('id', None)
         name = category.get('title', None)
-        if id!=None and name!=None:
+        if id and name:
             thumbnailImage = 'http://dyn.netzkino.de/wp-content/themes/netzkino/imgs/categories/'+str(id)+'.png'
             if id==8 or id==161:
                 thumbnailImage = __ICON_HIGHLIGHTS__
             params = {'action': __ACTION_SHOW_CATEGORY__,
                       'id': id}
-            bromixbmc.addDir(name=name, params=params, thumbnailImage=thumbnailImage, fanart=__FANART__)
+            __plugin__.addDirectory(name=name, params=params, thumbnailImage=thumbnailImage, fanart=__FANART__)
 
 def showIndex():
     params = {'action': __ACTION_SEARCH__}
-    bromixbmc.addDir("[B]"+bromixbmc.Addon.localize(30000)+"[/B]", params = params, thumbnailImage=__ICON_SEARCH__, fanart=__FANART__)
+    __plugin__.addDirectory("[B]"+__plugin__.localize(30000)+"[/B]", params = params, thumbnailImage=__ICON_SEARCH__, fanart=__FANART__)
     
     categories = _request('/capi-2.0a/index.json')
     
@@ -85,36 +82,31 @@ def showIndex():
     categories = categories.get('categories', {})
     _listCategories(categories)
             
-    xbmcplugin.endOfDirectory(bromixbmc.Addon.Handle)
-    return True
+    __plugin__.endOfDirectory()
 
 def _listPosts(json_posts):
-    def _getCleanPlot(plot):
-        result = re.sub('<[^<]+?>', '', plot)
-        return result
-    
-    xbmcplugin.setContent(bromixbmc.Addon.Handle, 'movies')
+    __plugin__.setContent('movies')
     for post in json_posts:
         id = post.get('id', None)
         name = post.get('title', None)
         thumbnailImage = post.get('thumbnail', '')
-        plot = _getCleanPlot(post.get('content', ''))
+        plot = bromixbmc.stripHtmlFromText(post.get('content', ''))
         
         custom_fields = post.get('custom_fields', None)
-        if custom_fields!=None:
+        if custom_fields:
             fanart = ''
             if __SETTING_SHOW_FANART__:
                 fanart = custom_fields.get('featured_img_all', [__FANART__])[0]
             streaming = custom_fields.get('Streaming', None)
-            if streaming!=None and len(streaming)>0:
+            if streaming and len(streaming)>0:
                 streamId = streaming[0]
                 params = {'action': __ACTION_PLAY__,
                           'id': streamId}
                 
-                additionalInfoLabels = {'plot': plot}
+                infoLabels = {'plot': plot}
                 year = custom_fields.get('Jahr', [])
                 if len(year)>0:
-                    additionalInfoLabels['year'] = year[0]
+                    infoLabels['year'] = year[0]
                     
                 cast = custom_fields.get('Stars', [])
                 if len(cast)>0:
@@ -122,36 +114,41 @@ def _listPosts(json_posts):
                     _cast = []
                     for c in cast:
                         _cast.append(c.strip())
-                    additionalInfoLabels['cast'] = _cast
+                    infoLabels['cast'] = _cast
                     
                 director = custom_fields.get('Regisseur', [])
                 if len(director)>0:
-                    additionalInfoLabels['director'] = director[0]
+                    infoLabels['director'] = director[0]
                     
-                rating = custom_fields.get('IMDb-Bewertung', ['0,0'])
-                if len(rating)>0:
-                    additionalInfoLabels['rating']=float(rating[0].replace(',', '.'))
+                ratings = custom_fields.get('IMDb-Bewertung', ['0,0'])
+                for rating in ratings:
+                    try:
+                        rating = rating.replace(',', '.')
+                        rating = float(rating)
+                        infoLabels['rating']=rating
+                        break
+                    except:
+                        # do nothing
+                        pass
                 
-                bromixbmc.addVideoLink(name=name, params=params, thumbnailImage=thumbnailImage, fanart=fanart, additionalInfoLabels=additionalInfoLabels)
+                __plugin__.addVideoLink(name=name, params=params, thumbnailImage=thumbnailImage, fanart=fanart, infoLabels=infoLabels)
 
 def showCategory(id):
     category = _request('/capi-2.0a/categories/'+id)
     posts = category.get('posts', {})
     _listPosts(posts)
     
-    xbmcplugin.endOfDirectory(bromixbmc.Addon.Handle)
-    return True
+    __plugin__.endOfDirectory()
 
 def play(id):
     url = 'http://netzkino_and-vh.akamaihd.net/i/'+id+'.mp4/master.m3u8'
-    listitem = xbmcgui.ListItem(path=url)
-    xbmcplugin.setResolvedUrl(bromixbmc.Addon.Handle, True, listitem)
+    __plugin__.setResolvedUrl(url)
     
 def search():
     success = False
-    keyboard = xbmc.Keyboard('', bromixbmc.Addon.localize(30000))
-    keyboard.doModal()
-    if keyboard.isConfirmed() and keyboard.getText():
+    
+    keyboard = bromixbmc.Keyboard(__plugin__.localize(30000))
+    if keyboard.doModal():
         success = True
         
         search_string = keyboard.getText().replace(" ", "+")
@@ -159,17 +156,16 @@ def search():
         posts = result.get('posts', [])
         _listPosts(posts)
         
-    xbmcplugin.endOfDirectory(bromixbmc.Addon.Handle, succeeded=success)
-    return True
+    __plugin__.endOfDirectory(success)
 
 action = bromixbmc.getParam('action')
 id = bromixbmc.getParam('id')
 
 if action == __ACTION_SEARCH__:
     search()
-elif action == __ACTION_SHOW_CATEGORY__ and id!=None:
+elif action == __ACTION_SHOW_CATEGORY__ and id:
     showCategory(id)
-elif action == __ACTION_PLAY__ and id!=None:
+elif action == __ACTION_PLAY__ and id:
     play(id)
 else:
     showIndex()
